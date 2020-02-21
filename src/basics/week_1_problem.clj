05ba0a5528144c681288b5d509f7aa3b6b31ab85
nagisaVon <liuchang62166@hotmail.com>
2020-02-20 21:48:14 -0500

    #28 and #29 on 4Clojure

--- src/basics/week_1_problem.clj
+++ src/basics/week_1_problem.clj
@@ -4,9 +4,9 @@
 
 (def year 2000)
 
-(if  (and (= (mod year 4) 0) (not= (mod year 100) 0))
+(if (and (= (mod year 4) 0) (not= (mod year 100) 0))
   (* 366 24 60 60)
-  (* 365 24 60 60) )
+  (* 365 24 60 60))
 
 ;; sum of 1+2+...+n
 
--- src/fourclojure/week4.clj
+++ src/fourclojure/week4.clj
@@ -1,4 +1,4 @@
-(ns four-clojure.week4)
+(ns fourclojure.week4)
 
 ;; Template
 ;;;;;;;;;;;;;;;;;;;;;;
@@ -27,6 +27,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; #28 Flatten a Sequence
 ;; input: (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+;; result: not working, no idea
+;;;;;;;;;;;;;;;;;;;;;;
+
+((fn [new-list arg]
+  (for [item arg]
+      (conj new-list item))) '() '(1 2 (3)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; #29 Get the Caps
+;; input: (= (__ "HeLlO, WoRlD!") "HLOWRD")
 ;; result: passed
 ;;;;;;;;;;;;;;;;;;;;;;
+
+((fn[mystr] (apply str (filter #(Character/isUpperCase %) mystr))) "DfjdjdiJDJD" )
+
 
